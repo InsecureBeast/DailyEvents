b3a74e9e60fdc49f36c68ff63bdd9ee965dbabd0
Dmitriev <Dmitriev@localhost>
2016-12-24 22:54:42 +0300

    Event item style

---
+++ KudaGo.Client/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace KudaGo.Client.Converters
+{
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = value as string;
+            if (val == null)
+                return Visibility.Collapsed;
+
+            return string.IsNullOrEmpty(val) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture.EnglishName);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
