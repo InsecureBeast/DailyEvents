d6314d59dd880430b7bc0b500540d8ad0aa987ce
Dmitriev <Dmitriev@localhost>
2017-01-06 01:16:34 +0300

    ????? ??????? ??? ????????? ??????? ????.

--- KudaGo.Client/ViewModels/Details/SelectionDetailsNodeViewModel.cs
+++ KudaGo.Client/ViewModels/Details/SelectionDetailsNodeViewModel.cs
@@ -5,9 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using KudaGo.Core.Selections;
 using KudaGo.Client.Extensions;
-using KudaGo.Core.Data;
 using KudaGo.Client.Model;
-using KudaGo.Client.Helpers;
 
 namespace KudaGo.Client.ViewModels.Details
 {
@@ -19,29 +17,15 @@ namespace KudaGo.Client.ViewModels.Details
                 return;
 
             Image = item.FirstImage.Thumbnail.Small;
+            NormalImage = item.FirstImage.Thumbnail.Normal;
             Title = item.Title.GetNormalString();
             Description = item.Description;
-
-            //LayoutHelper.InvokeFromUiThread(async () => await LoadImages(item, dataSource));
         }
 
         public string Image { get; private set; }
+        public string NormalImage { get; private set; }
         public string Title { get; private set; }
         public string Description { get; private set; }
         public string Place { get; private set; }
-
-        private async Task LoadImages(ISelectionItem item, DataSource dataSource)
-        {
-            if (item.CType == "place")
-            {
-                var rs = await dataSource.GetPlaceDetails(item.Id);
-                var image = rs.Images.FirstOrDefault();
-                if (image == null)
-                    return;
-
-                Image = image.Thumbnail.Small;
-            }
-
-        }
     }
 }
