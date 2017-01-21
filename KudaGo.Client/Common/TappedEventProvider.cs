09c5a4e932f32278b34aafbaf4a99a77ac3e7a1e
Dmitriev <Dmitriev@localhost>
2017-01-21 18:14:09 +0300

    Tap to map

---
+++ KudaGo.Client/Common/TappedEventProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace KudaGo.Client.Common
+{
+    public class TappedEventProvider
+    {
+        private static FrameworkElement _element;
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached("Command", typeof(ICommand), typeof(TappedEventProvider), new PropertyMetadata(null, OnCommandChange));
+
+        public static void SetCommand(DependencyObject element, ICommand value)
+        {
+            element.SetValue(CommandProperty, value);
+        }
+
+        public static ICommand GetCommand(DependencyObject element)
+        {
+            return (ICommand)element.GetValue(CommandProperty);
+        }
+
+        private static void OnCommandChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            _element = d as FrameworkElement;
+            if (_element == null)
+                return;
+
+            _element.Tapped += Element_Tapped;
+        }
+
+        private static void Element_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            var command = GetCommand(_element);
+            if (command == null)
+                return;
+
+            //TODO command parametr
+            command.Execute(null);
+        }
+    }
+}
