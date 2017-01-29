7d62884f3eaf41b9fd85e052f4c4a0ca602d6bf4
Dmitriev <Dmitriev@localhost>
2017-01-29 18:19:11 +0300

    ????????? ???????? ? ??????????? ????????

---
+++ KudaGo.Client/Converters/StringToGridColumnSpanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace KudaGo.Client.Converters
+{
+    class StringToGridColumnSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var str = value as string;
+            if (str == null)
+                return 2;
+
+            if (string.IsNullOrEmpty(str))
+                return 2;
+
+            return 1;                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
